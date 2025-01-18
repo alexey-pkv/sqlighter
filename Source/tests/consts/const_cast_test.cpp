c03b42d2a9d70e1e361cf9ad9bb7268e07f3cd91
Alexey Puchkov <alexey.pkv@gmail.com>
2025-01-18 15:28:40 +0200

    Add some tests

--- Source/tests/consts/const_cast_test.cpp
+++ Source/tests/consts/const_cast_test.cpp
@@ -40,3 +40,40 @@ TEST(consts, const_cast__OrderBy_InvalidValue__exception_thrown)
 		ASSERT_EQ(SQLIGHTER_ERR_INVALID_ENUM, e.code());
 	}
 }
+
+TEST(consts, const_cast__ScalarValue_type__invalid_value__exception_thrown)
+{
+	try
+	{
+		std::ostringstream ss;
+		ss << (sqlighter::ScalarValue::type)(13);
+		FAIL();
+	}
+	catch (const sqlighter::SQLighterException& e)
+	{
+		ASSERT_EQ(SQLIGHTER_ERR_INVALID_ENUM, e.code());
+	}
+}
+
+TEST(consts, const_cast__ScalarValue_type)
+{
+	std::ostringstream s1;
+	std::ostringstream s2;
+	std::ostringstream s3;
+	std::ostringstream s4;
+	std::ostringstream s5;
+	
+	
+	s1 << sqlighter::ScalarValue::type::INT;
+	s2 << sqlighter::ScalarValue::type::DOUBLE;
+	s3 << sqlighter::ScalarValue::type::NULL_VAL;
+	s4 << sqlighter::ScalarValue::type::TEXT;
+	s5 << sqlighter::ScalarValue::type::BLOB;
+	
+	
+	ASSERT_EQ("INT_64",		s1.str());
+	ASSERT_EQ("DOUBLE",		s2.str());
+	ASSERT_EQ("NULL_VAL",	s3.str());
+	ASSERT_EQ("TEXT",		s4.str());
+	ASSERT_EQ("BLOB",		s5.str());
+}
