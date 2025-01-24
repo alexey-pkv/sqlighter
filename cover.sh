d9b08350c4d0934f17c3802d2e3d2fba0915e8a2
Alexey Puchkov <alexey.pkv@gmail.com>
2025-01-24 20:52:52 +0200

    Make more friendly for cmake fetch

--- cover.sh
+++ cover.sh
@@ -1,4 +1,4 @@
-cmake -S . -B build/cover -DBUILD_FOR_COVER=true
+cmake -S . -B build/cover -DSQLIGHTER_BUILD_FOR_COVER=true
 cmake --build build/cover --target cover -- -j10
 
 echo "file://$(dirname "$(realpath "$0")")/build/cover/coverage/index.html"
