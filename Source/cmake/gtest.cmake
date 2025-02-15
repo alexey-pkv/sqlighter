33cf9ace8409c6c6f18ccb7a94ea667640ee267b
Alexey Puchkov <alexey.pkv@gmail.com>
2025-02-15 11:21:46 +0200

    Minor updates to build process

--- Source/cmake/gtest.cmake
+++ Source/cmake/gtest.cmake
@@ -1,30 +1,19 @@
 include(FetchContent)
 
 
+message(STATUS "Fetching GTest 1.16.0")
+
+
 enable_testing()
 
 
-find_package(GTest CONFIG QUIET)
+FetchContent_Declare(
+	googletest
+	GIT_REPOSITORY	https://github.com/google/googletest.git
+	GIT_TAG			v1.16.0
+)
 
-
-if(GTest_FOUND)
-	
-	message(STATUS "Found GTest")
-
-else()
-	
-	message(STATUS "GTest not found. Fetching 1.16.0")
-	
-	include(FetchContent)
-	
-	FetchContent_Declare(
-		googletest
-		GIT_REPOSITORY	https://github.com/google/googletest.git
-		GIT_TAG			v1.16.0
-	)
-	
-	FetchContent_MakeAvailable(googletest)
-endif()
+FetchContent_MakeAvailable(googletest)
 
 
 set(GTEST_INCLUDE_DIR "${gtest_SOURCE_DIR}/include" CACHE PATH "Path to GTest include directory")
--- Source/cmake/sqlite.cmake
+++ Source/cmake/sqlite.cmake
@@ -1,40 +1,32 @@
-find_package(SQLite3 CONFIG QUIET)
+include(FetchContent)
 
 
-if(SQLite3_FOUND)
-	
-	message(STATUS "Found SQLite3: ${SQLite3_INCLUDE_DIRS}, ${SQLite3_LIBRARIES}")
-	
+message(STATUS "Fetching SQLite 3.49.0")
+
+
+FetchContent_Declare(
+	sqlite3
+	URL https://www.sqlite.org/2025/sqlite-amalgamation-3490000.zip
+	URL_HASH SHA3_256=138b6922eb274c953c9d8f3249c39706ad3e40f3eef5d81bbeb73509b41f52c9
+)
+
+FetchContent_MakeAvailable(sqlite3)
+
+if(EXISTS "${sqlite3_SOURCE_DIR}/sqlite3.h")
+	message(STATUS "Found sqlite3.h at ${sqlite3_SOURCE_DIR}/sqlite3.h")
 else()
-	
-	message(STATUS "SQLite3 not found. Fetching 3.49")
-	
-	include(FetchContent)
-	
-	FetchContent_Declare(
-		sqlite3
-		URL https://www.sqlite.org/2025/sqlite-amalgamation-3490000.zip
-		URL_HASH SHA3_256=138b6922eb274c953c9d8f3249c39706ad3e40f3eef5d81bbeb73509b41f52c9
-	)
-	
-	FetchContent_MakeAvailable(sqlite3)
-	
-	if(EXISTS "${sqlite3_SOURCE_DIR}/sqlite3.h")
-		message(STATUS "Found sqlite3.h at ${sqlite3_SOURCE_DIR}/sqlite3.h")
-	else()
-		message(FATAL_ERROR "sqlite3.h not found in ${sqlite3_SOURCE_DIR}")
-	endif()
-	
-	
-	if (UNIX)
-		add_library(sqlite3 SHARED ${sqlite3_SOURCE_DIR}/sqlite3.c)
-		target_link_libraries(sqlite3 PRIVATE dl)
-	else () 
-		add_library(sqlite3 STATIC ${sqlite3_SOURCE_DIR}/sqlite3.c)
-	endif ()
-	
-	target_include_directories(sqlite3 PUBLIC ${sqlite3_SOURCE_DIR})
+	message(FATAL_ERROR "sqlite3.h not found in ${sqlite3_SOURCE_DIR}")
 endif()
+
+
+if (UNIX)
+	add_library(sqlite3 SHARED ${sqlite3_SOURCE_DIR}/sqlite3.c)
+	target_link_libraries(sqlite3 PRIVATE dl)
+else () 
+	add_library(sqlite3 STATIC ${sqlite3_SOURCE_DIR}/sqlite3.c)
+endif ()
+
+target_include_directories(sqlite3 PUBLIC ${sqlite3_SOURCE_DIR})
 
 
 set(SQLITE3_SOURCE_DIR ${sqlite3_SOURCE_DIR} CACHE PATH "Path to SQLite3 source directory")
